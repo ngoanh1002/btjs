4d713e899d9199675797a1bdea1ddeb562e1d2bc
ngoanh1002 <116912494+ngoanh1002@users.noreply.github.com>
2023-06-20 23:33:33 +0700

    new

--- index.js
+++ index.js
@@ -1,212 +1,235 @@
-// XỬ LÝ SỐ
-//Bài1
-function the_tich_hinh_cau(r) {
+/**
+ * Xử lý số
+ */
 
-    const result = 4 / 3 * Math.PI * Math.pow(r, 3)
+// Bài 1
+function sphericalVolumn(r) {
+    let total = 4 / 3 * Math.PI * Math.pow(r, 3);
+    return total;
+}
+
+console.log('thể tích hình cầu là ' + sphericalVolumn(6));
+
+// Bài 2
+function totalBetween(n) {
+    let {n1, n2} = n;
+    let numberBetween = [];
+    if (n1 < n2) {
+        for (let number = n1 + 1; number < n2; number++ ) {
+            numberBetween.push(number);
+        }
+    }
+
+    if (n1 > n2) {
+        for (let number = n2 + 1; number < n1; number++ ) {
+            numberBetween.push(number);
+        }
+    }
+
+    let result = numberBetween.reduce((a, v) => {
+        return a + v;
+    }, 0)
     console.log(result)
 }
 
+totalBetween({
+    n1: 8,
+    n2: 3
+});
 
-
-//bai 2
-function tong_cac_so_nam_giua(number_1, number_2) {
-    let bai_1_2 = 0
-    let arr = []
-    if (number_1 < number_2) {
-        for (let i = number_1 + 1; i < number_2; i++) {
-            arr.push(i)
-        }
-        for (const item of arr) {
-            bai_1_2 += item
-        }
-        console.log(bai_1_2)
-    }
-    if (number_1 > number_2) {
-        for (i = number_2 + 1; i < number_1; i++) {
-            arr.push(i)
-        }
-        for (const item of arr) {
-            bai_1_2 += item
-        }
-        console.log(bai_1_2)
-    }
-}
-
-
-//bai 3
-function tong_cac_uoc_so(number) {
-    let arr = []
-    let bai_1_3 = 0
+// Bài 3
+function divisorSum(number) {
+    let arr = [];
+    let total = 0;
     for (let i = 1; i <= number; i++) {
         if (number % i === 0) {
-            arr.push(i)
+            arr.push(i);
         }
     }
 
     for (const numb of arr) {
-        bai_1_3 += numb
+        total += numb
     }
-    console.log(bai_1_3)
+    console.log(total);
 }
 
+divisorSum(4);
 
-
-//bai 4
-function kiem_tra_snt(number) {
-    let bai_1_4
+// Bài 4
+function primeNumberCheck(number) {
+    let primeNumber
     if (number <= 1) return false
     else {
         for (let i = 2; i < number; i++) {
             if (number % i === 0) {
-                bai_1_4 = false
-                break
+                primeNumber = false;
+                break;
             }
             else {
-                bai_1_4 = true
+                primeNumber = true;
             }
         }
     }
-    console.log(bai_1_4)
+    console.log(primeNumber);
 }
 
+primeNumberCheck(3);
 
-
-//bai 5
-function isNT(number) {
+// Bài 5
+function primeNumber(number) {
     for (let i = 2; i < number; i++) {
         if (number % i === 0) {
-            return false
+            return false;
         }
-        // else{
-        //     return true
-        // }
     }
-    return true
+    return true;
 }
 
-function tong_cac_so_nguyen_to(number) {
-    let sum = 0
+function primeNumberSum(number) {
+    let sum = 0;
     for (let index = 2; index <= number; index++) {
-        if (isNT(index)) sum += index
+        if (primeNumber(index)) sum += index;
 
     }
-    console.log(sum)
+    console.log(sum);
 }
 
+primeNumberSum(5);
 
 
-/*XỬ LÝ CHUỖI*/
-//bai 1
-let str = 'HELLO World'
-let toLower = str.toLowerCase()
-let arrChars = toLower.split('')
-let newArr = []
-newArr.push(arrChars[0].toUpperCase())
-for (let i = 1; i < arrChars.length; i++) {
+/**
+ * Xử lý chuỗi
+ */
 
-    if (arrChars[i - 1] === ' ') {
-        newArr.push(arrChars[i].toUpperCase())
+// Bài 1
+function upperCaseSentence(sentence) {
+    let sntnArr = sentence.split(' ');
+    let newSentence = [];
+    for (let word of sntnArr) {
+        firstLetter = word.slice(0, 1);
+        firstLetter = firstLetter.toUpperCase();
+        restOfWord = word.slice(1, word.length);
+        restOfWord = restOfWord.toLowerCase();
+        word = firstLetter + restOfWord;
+        
+        newSentence.push(word);
     }
-    else {
-        newArr.push(arrChars[i])
-    }
-
+    console.log(newSentence.join(' '));
 }
-newArr.join('')
+
+upperCaseSentence('HELLO my NaMe iS LINH');
 
 
-//bai 2
-function thay_doi_gach_ngang() {
-    let str = 'HELLO World'
-    let toLower = str.toLowerCase()
-    let arrChars = toLower.split('')
-    let newArr_2 = []
-    for (let i = 0; i < arrChars.length; i++) {
-        if (arrChars[i] === ' ') {
-            arrChars[i] = '-'
-            newArr_2.push(arrChars[i])
+// Bài 2
+function spinalCase(sentence) {
+    let newSentence = sentence.toLowerCase()
+    let sntnArr = newSentence.split(' ');
+    console.log(sntnArr.join('-'));
+}
+
+spinalCase('HELLO my NaMe iS LINH');
+
+// Bài 3
+function palindrome(sentence) {
+    let newSntnArr = sentence.split(' ').join('');
+    console.log(newSntnArr);
+
+    let newArr = newSntnArr.split('');
+    let reverseSntn = newArr.reverse().join('');
+    console.log(reverseSntn);
+
+    console.log(newSntnArr === reverseSntn)
+}
+
+palindrome('hello world');
+
+// Bài 4
+function containedSentence(p) {
+    let {s1, s2} = p;
+    let newS1 = s1.toLowerCase().split('');
+    let newS2 = s2.toLowerCase().split('');
+
+    console.log(newS1);
+    console.log(newS2);
+
+    if (newS1.length >= newS2.length) {
+        let checker = (arr, target) => target.every(v => arr.includes(v));
+        console.log(checker(newS1, newS2));
+    }
+
+    if (newS1.length < newS2.length) {
+        let checker = (arr, target) => target.every(v => arr.includes(v));
+        console.log(checker(newS2, newS1));
+    }
+}
+
+containedSentence({
+    s1: 'Hello World',
+    s2: 'Hell '
+});
+
+
+/**
+ * Xử lý mảng
+ */
+
+// Bài 1
+function filter_array(test_array) {
+    var index = -1,
+        arr_length = test_array ? test_array.length : 0,
+        resIndex = -1,
+        result = [];
+
+    while (++index < arr_length) {
+        var value = test_array[index];
+
+        if (value) {
+            result[++resIndex] = value;
         }
-        else {
-            newArr_2.push(arrChars[i])
+    }
+
+    console.log(result);
+}
+filter_array([NaN, 0, 15, false, -22, '',undefined, 47, null]);
+
+// Bài 2
+function longestString(arr) {
+    let length = 0;
+    let longestString = [];
+    for (let item of arr) {
+        if (item.length > length) {
+            length = item.length;
+        }
+
+    }
+    for (let item of arr) {
+        if (item.length === length) {
+            longestString.push(item);
         }
     }
-    const result = newArr_2.join('')
+    console.log(longestString);
 }
 
+longestString(['aba', 'aa', 'ad', 'c', 'vcd'] );
 
+// Bài 3
+function differentNumber(p) {
+    let {arr1, arr2} = p;
+    let newArr = [];
+    for (let n1 of arr1) {
+        for (let n2 of arr2) {
+            if (arr1[n1] == arr2[n2]) {
+                return true;
+            }
+        }
+    }
 
+    return false;
+}
 
-
-//bai 3
-//bai 4
-
-
-
-
-/*XỬ LÝ MẢNG*/
-//bai 1
-function loai_gia_tri_sai() {
-    let arr_bai_1 = [1, 2, 3, undefined, null, '', false]
-    let arr_result = arr_bai_1.filter(function (x) {
-
-        if (!!x === true) return true
-        return false
+console.log(
+    differentNumber({
+        arr1: [1, 2, 3],
+        arr2: [1, 3, 4, 5, 5] 
     })
-}
-
-
-//bai 2
-function tim_chuoi_dai_nhat() {
-    let arr = ['aaa', 'bb', 'cc', 'd', '123']
-    let max_length = 0
-    let result = []
-    for (const item of arr) {
-        if (item.length > max_length) {
-            max_length = item.length
-        }
-
-    }
-    for (const item of arr) {
-        if (item.length === max_length) {
-            result.push(item)
-        }
-    }
-    console.log(result)
-}
-tim_chuoi_dai_nhat()
-
-//bai 3
-
-
-/*XỬ LÝ OBJECT*/
-//bai 1
-function tuoi_trung_binh(arr) {
-
-    let sum = 0
-    for (const item of arr) {
-        sum += item.age
-
-    }
-    const result = sum / arr.length
-}
-
-//bai 2
-function sap_xep_ten_theo_tuoi(arr) {
-    const result = arr.sort(function (a, b) {
-
-
-
-        if (a.age < b.age) return 1
-        else return -1
-
-
-    })
-    console.log(result)
-}
-
-
-/*TONG HOP*/
-//bai 1
-function thoi_gian(time, x) {
-
-}
+)
